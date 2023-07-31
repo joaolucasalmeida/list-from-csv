c7e11b8fb96f5e55f064aa5c846ef8ecc9aa5647
Joao Almeida <joaolucas.alm@gmail.com>
2023-07-31 00:49:49 -0300

    Add frontend error handler

--- front-end/src/services/UserService.js
+++ front-end/src/services/UserService.js
@@ -1,23 +1,34 @@
 import axios from "axios";
+import { NotificationManager } from 'react-notifications';
 
 class UserService {
   static async findUsers(query) {
     try {
-        return await axios.get(`http://localhost:4000/api/users?q=${query}`);
-      } catch (error) {
-        console.error('Error fetching data: ', error);
+      return await axios.get(`http://localhost:4000/api/users?q=${query}`);
+    } catch (error) {
+      console.error('Error fetching data: ', error);
+      if (error.response.status === 500) {
+        NotificationManager.error('', 'Erro ao buscar dados', 3000, () => {
+          alert('callback');
+        });
       }
+    }
   }
 
   static async uploadFile(body) {
     try {
-        return await axios.post('http://localhost:4000/api/files', body, {
-            headers: {
-                'Content-Type': 'multipart/form-data'
-            },
-        });
+      return await axios.post('http://localhost:4000/api/files', body, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        },
+      });
     } catch (error) {
-        console.log('Upload Error', error);
+      console.log('Upload Error', error);
+      if (error.response.status === 500) {
+        NotificationManager.error('', 'Erro ao realizar upload', 3000, () => {
+          alert('callback');
+        });
+      }
     }
   }
 }
